56ef81ad825712338ed8cecc335cc66711462a5a
Farhan <farhannizami0612@gmail.com>
2024-05-06 01:08:33 +0600

    add and delete job functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,46 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
-import JobPage from './pages/JobPage';
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout></MainLayout>}>
-      <Route index element={<HomePage></HomePage>}></Route>
-      <Route path='/jobs' element={<JobsPage></JobsPage>}></Route>
-      <Route path='/jobs/:id' element={<JobPage></JobPage>}></Route>
-      <Route path='*' element={<NotFoundPage></NotFoundPage>}></Route>
-    </Route>
-  )
-);
+
+
 
 const App = () => {
+
+  // Add new job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return;
+  }
+
+  // Delete Job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return;
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout></MainLayout>}>
+        <Route index element={<HomePage></HomePage>}></Route>
+        <Route path='/jobs' element={<JobsPage></JobsPage>}></Route>
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}></AddJobPage>}></Route>
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob}></JobPage>} loader={jobLoader}></Route>
+        <Route path='*' element={<NotFoundPage></NotFoundPage>}></Route>
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router}></RouterProvider>
 }
 
