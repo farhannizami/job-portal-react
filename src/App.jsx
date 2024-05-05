5b3ed5efed120659c6fd537ecc6ead8f8a864e81
Farhan <farhannizami0612@gmail.com>
2024-05-05 22:24:30 +0600

    proxy added, jobpage skeleton done

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage from './pages/JobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout></MainLayout>}>
       <Route index element={<HomePage></HomePage>}></Route>
       <Route path='/jobs' element={<JobsPage></JobsPage>}></Route>
+      <Route path='/jobs/:id' element={<JobPage></JobPage>}></Route>
       <Route path='*' element={<NotFoundPage></NotFoundPage>}></Route>
     </Route>
   )
